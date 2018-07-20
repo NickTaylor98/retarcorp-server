f6a3b69895d4471599af6792fef64a6c8ebdbfb0
Nikita Laptev <nikita.laptev98@inbox.ru>
2018-07-20 18:30:48 +0300

    format code for checking authorization

--- helpers/authorization.js
+++ helpers/authorization.js
@@ -1,20 +1,22 @@
 'use strict';
+
+const checkArray = (array, ability, action) => array.reduce((rc, element) => rc && ability.can(action, element), true);
+
+const checkOneTask = (obj, ability, action) => {
+    const rc = obj.users_tasks.reduce((rc, elem) => {
+        obj.dataValues.users_tasks = elem;
+        return rc || ability.can(action, obj);   
+    }, false);
+    delete obj.dataValues.users_tasks;
+    return rc;
+}
+
+const checkSimpleObject = (obj, ability, action) => ability.can(action, obj);
+
 module.exports.checkAuth = async (ability, action, obj) => {
-    if (Array.isArray(obj)) {
-        let rc = true;
-        obj.forEach(element => {
-            rc = rc && ability.can(action, element);
-        });    
-        return rc;
-    } else if (Array.isArray(obj.users_tasks)) //for checking one task 
-        {
-            let rc = false;
-            obj.users_tasks.forEach(elem => {
-                obj.dataValues.users_tasks = elem;
-                rc = rc || ability.can(action, obj);   
-            });
-            delete obj.dataValues.users_tasks;
-            return rc;
-        }
-    else return ability.can(action, obj)
+    if (Array.isArray(obj)) 
+        return checkArray(obj, ability, action);
+    else if (Array.isArray(obj.users_tasks)) 
+        return checkOneTask(obj, ability, action);
+    else return checkSimpleObject(obj, ability, action);
 }
